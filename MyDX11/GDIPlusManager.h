451bef6d657cbfdc24bbbdbef303c3337ea1e08f
SakuraiNOOB2 <52180510+SakuraiNOOB2@users.noreply.github.com>
2020-11-27 12:13:48 +0900

    Texture Mapping

---
+++ MyDX11/GDIPlusManager.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "myWin.h"
+
+
+class GDIPlusManager {
+
+public:
+	GDIPlusManager();
+	~GDIPlusManager();
+private:
+	static ULONG_PTR token;
+	static int refCount;
+
+};
--- MyDX11/app.cpp
+++ MyDX11/app.cpp
@@ -2,9 +2,15 @@
 #include "Melon.h"
 #include "Pyramid.h"
 #include "Box.h"
+#include "Sheet.h"
+#include "SkinnedBox.h"
 #include <memory>
 #include <algorithm>
 #include "myMath.h"
+#include "Surface.h"
+#include "GDIPlusManager.h"
+
+GDIPlusManager gdipm;
 
 #define windowLenth (1024)
 #define windowWidth (768)
@@ -44,6 +50,17 @@ App::App()
 					gfx, rng, adist, ddist, odist, rdist, longdist, latdist
 					);
 
+			case 3:
+				return std::make_unique<Sheet>(
+					gfx, rng, adist, ddist, odist, rdist
+					);
+
+			case 4:
+				return std::make_unique<SkinnedBox>(
+					gfx, rng, adist, ddist, odist, rdist
+					);
+
+
 			default:
 
 				assert(false && "Bad drawable type in factory");
@@ -64,13 +81,12 @@ App::App()
 		std::uniform_real_distribution<float> bdist{ 0.4f,3.0f };
 		std::uniform_int_distribution<int> latdist{ 5,20 };
 		std::uniform_int_distribution<int> longdist{ 10,40 };
-		std::uniform_int_distribution<int> typedist{ 0,2 };
+		std::uniform_int_distribution<int> typedist{ 0,4 };
 	};
 
 	//create boxes
-	Factory f(wnd.Gfx());
 	drawables.reserve(nDrawables);
-	std::generate_n(std::back_inserter(drawables), nDrawables, f);
+	std::generate_n(std::back_inserter(drawables), nDrawables, Factory{ wnd.Gfx() });
 
 	//set the projection matrix for the graphics
 	wnd.Gfx().SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
@@ -104,7 +120,7 @@ void App::DoFrame() {
 	for (auto& d:drawables) {
 
 		//update all the boxes
-		d->Update(dt);
+		d->Update(wnd.kbd.KeyIsPressed(VK_SPACE) ? 0.0f : dt);
 
 		//draw all the boxes
 		d->Draw(wnd.Gfx());
